236167d6f316c611cb868d80fbe12b71bb30662c
Jonathan Nguyen <njonathan990@gmail.com>
2022-03-28 18:51:02 -0500

    updated stretch goal #8

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -95,15 +95,16 @@ console.log('should say false', find(711, list1));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-  for( let i=0; i<string.length; i++){
+  i=0;
     if( letter === string[i]){
       return true;
     }
+    return false;
   }
-  return false;
-}
+
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log('another test - should say true', isFirstLetter('b', 'bacon'));
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
